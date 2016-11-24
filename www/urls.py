c510c45d08c79715955021b05855b0e6ae89f5fc
bingo-chou <bingochou@foxmail.com>
2016-11-24 16:16:28 +0800

    add register and signin function

--- www/urls.py
+++ www/urls.py
@@ -1,10 +1,62 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import os,re,time,base64,hashlib,logging
+import os,re,time,base64,hashlib
+import logging;logging.basicConfig(level=logging.DEBUG)
 from transwarp.web import get,view,post,ctx,interceptor,seeother,notfound
 from apis import api,APIError,APIValueError,APIPermissionError,APIResourceNotFoundError
 from models import User,Blog,Comment
+from config import configs
+
+_COOKIE_NAME='awesession'
+_COOKIE_KEY=configs.session.secret
+
+def make_signed_cookie(id,password,max_age):
+	#以id-password-expires形式生成cookie
+	expires=str(int(time.time()+(max_age or 86400)))
+	L=[id,expires,hashlib.md5('%s-%s-%s-%s'%(id,password,expires,_COOKIE_KEY)).hexdigest()]
+	return '-'.join(L)
+
+def parse_signed_cookie(cookie_str):
+	try:
+		L=cookie_str.split('-')
+		if len(L)!=3:
+			return None
+		id,expires,md5=L
+		if int(expires)<time.time():
+			return None
+		user=User.get(id)
+		if user is None:
+			return None
+		if md5!=hashlib.md5('%s-%s-%s-%s'%(id,user.password,expires,_COOKIE_KEY)).hexdigest():
+			return None
+		return user
+	except:
+		return None
+def check_admin():
+	user=ctx.request.user
+	if user and user.admin:
+		return 
+	raise APIPermissionError('No permission.')
+
+@interceptor('/')
+def user_interceptor(next):
+	logging.info('try to bind user from session cookie...')
+	user=None
+	cookie=ctx.request.cookies.get(_COOKIE_NAME)
+	if cookie:
+		logging.info('parse session cookie...')
+		user=parse_signed_cookie(cookie)
+		if user:
+			logging.info('bind user <%s> to session...'%user.email)
+	ctx.request.user=user
+	return next()
+@interceptor('/manage/')
+def manage_interceptor(next):
+	user=ctx.request.user
+	if user and user.admin:
+		return next()
+	raise seeother('/signin')
 
 @view('blogs.html')
 @get('/')
@@ -13,6 +65,67 @@ def index():
 	user=User.find_first('where email=?','admin@example.com')
 	return dict(blogs=blogs,user=user)
 
+@view('signin.html')
+@get('/signin')
+def signin():
+	return dict()
+
+@get('/signout')
+def signout():
+	ctx.response.delete_cookie(_COOKIE_NAME)
+	raise seeother('/')
+
+@api
+@post('/api/authenticate')
+def authenticate():
+	#logging.info('authenticating-----------')
+	i=ctx.request.input(remember='')
+	email=i.email.strip().lower()
+	password=i.possword
+	remember=i.remember
+	user=User.find_first('where email=?',email)
+	if user is None:
+		raise APIError('auth:failed','email','Invalid email')
+	elif user.password!=password:
+		raise APIError('auth:failed','password','Invalid password')
+	#生成cookie
+	max_age=604800 if remember=='true' else None
+	cookie=make_signed_cookie(_COOKIE_NAME,cookie,max_age=max_age)
+	user.password='*'*6
+	return user
+
+_RE_EMAIL=re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_MD5=re.compile(r'^[0-9a-f]{32}$')
+
+@api
+@post('/api/users')
+def register_user():
+	i=ctx.request.input(name='',email='',password='')
+	name=i.name.strip()
+	email=i.email.strip().lower()
+	password=i.password
+	#logging.info('register test..........')
+	if not name:
+		raise APIValueError('name')
+	if not email or not _RE_EMAIL.match(email):
+		raise APIValueError('email')
+	if not password or not _RE_MD5.match(password):
+		raise APIValueError('password')
+	user=User.find_first('where email=?',email)
+	if user:
+		raise APIError('register:failed','email','Email is already in use.')
+	user=User(name=name,email=email,password=password,image='http://www.gravatar.com/avatar/%s?d=mm&s=120'%hashlib.md5(email).hexdigest())
+	user.insert()
+
+	#会话cookie
+	cookie=make_signed_cookie(user.id,user.password,None)
+	ctx.response.set_cookie(_COOKIE_NAME,cookie)
+	return user	
+@view('register.html')
+@get('/register')
+def register():
+	return dict()
+
 @api
 @get('/api/users')
 def api_get_users():
@@ -20,3 +133,6 @@ def api_get_users():
 	for u in users:
 		u.password='*'*6
 	return dict(users=users)
+
+
+
